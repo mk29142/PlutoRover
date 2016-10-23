1254288386477159add92b80340c59465b3c58b0
mk29142 <mk1514@ic.ac.uk>
2016-10-23 18:50:33 +0100

    Main now works with user input.

--- src/Main.java
+++ src/Main.java
@@ -10,25 +10,29 @@ public class Main {
 		PrintStream ps = new PrintStream(System.out);
 		Surface surface = new Surface(10, 10);
 		Rover rover = new NormalRover(0, 0, Direction.N, surface);
-		
-		while(scanner.hasNext()) {
-			String command = scanner.nextLine();
-			char[] instructions = command.toCharArray();
-			for(char i : instructions) {
-				switch (i) {
-				case 'F':
-					rover.move();
-					break;
-				case 'B':
-					rover.rotate(Rotation.LEFT);
-					rover.rotate(Rotation.RIGHT);
-					rover.move();
-					break;
-				default:
-					break;
-				}
+
+		String command = scanner.nextLine();
+		char[] instructions = command.toCharArray();
+		for (char i : instructions) {
+			System.out.println(i);
+			switch (i) {
+			case 'F':
+				rover.move();
+				break;
+			case 'B':
+				rover.rotate(Rotation.LEFT);
+				rover.rotate(Rotation.RIGHT);
+				rover.move();
+				break;
+			case 'R':
+				rover.rotate(Rotation.RIGHT);
+				break;
+			case 'L':
+				rover.rotate(Rotation.LEFT);
+				break;
 			}
 		}
+		ps.print(surface);
 	}
 
 }
